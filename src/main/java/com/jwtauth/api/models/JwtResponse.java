3b2b58e1642664825a86c504e5bd01df8f3a4f6c
PranitaJagdale1 <pranitajagdale9@gmail.com>
2022-08-27 19:50:03 +0530

    First commit

---
+++ src/main/java/com/jwtauth/api/models/JwtResponse.java
@@ -0,0 +1,25 @@
+package com.jwtauth.api.models;
+
+public class JwtResponse {
+
+	String Token;
+
+	public String getToken() {
+		return Token;
+	}
+
+	public void setToken(String token) {
+		Token = token;
+	}
+
+	public JwtResponse(String token) {
+		super();
+		Token = token;
+	}
+
+	public JwtResponse() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+}
